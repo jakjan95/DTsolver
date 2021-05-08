0560feae6c52d3280ee38f121ebd0bef1242b051
jakjan95 <jakubjanusz95@gmail.com>
2021-05-08 11:31:00 +0200

    Adult dataset added

--- data/data.py
+++ data/data.py
@@ -9,6 +9,10 @@ breast_cancer_data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databa
 #https://archive.ics.uci.edu/ml/datasets/Contraceptive+Method+Choice
 cmc_data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/cmc/cmc.data'
 
-wget.download(car_data_url)
-wget.download(breast_cancer_data_url)
-wget.download(cmc_data_url)
+#adult
+adult_data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
+
+# wget.download(car_data_url)
+# wget.download(breast_cancer_data_url)
+# wget.download(cmc_data_url)
+wget.download(adult_data_url)
